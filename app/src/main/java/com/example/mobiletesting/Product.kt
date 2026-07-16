025e8873f7b3c200d7e55aedde83acc795a92e60
sbarguesdev <sbargues.dev@gmail.com>
2026-07-16 00:16:50 -0300

    Add files via upload
    Agregar configuración del módulo app

---
+++ app/src/main/java/com/example/mobiletesting/Product.kt
@@ -0,0 +1,10 @@
+package com.example.mobiletesting
+
+data class Product(
+    val id: Int,
+    val name: String,
+    val description: String,
+    val price: Double,
+    val currency: String,
+    val inStock: Boolean
+)
---
+++ app/src/main/java/com/example/mobiletesting/ProductResponse.kt
@@ -0,0 +1,5 @@
+package com.example.mobiletesting
+
+data class ProductResponse(
+    val products: List<Product>
+)
